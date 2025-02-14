626c52acf66d008028ed5feb241fcdf9d5b5dd6c
yuanmingz7 <viinnnnniie@gmail.com>
2025-02-14 11:08:37 -0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,12 @@
+""" x = 3
+y = float(3)
+print(x,y)
+students = [{'name':'janet', 'siblings': 'jenny', 'name': 'amanda'}]
+            
+for student in students:
+    print(student['name'])
+ """
+values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
