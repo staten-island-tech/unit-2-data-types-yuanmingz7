00baeabb94b87f591a94d255c299f5d3401aa322
yuanmingz7 <viinnnnniie@gmail.com>
2025-02-26 10:14:45 -0500

    Create french.py

---
+++ french.py
@@ -0,0 +1,25 @@
+""" def french(t, T):
+    if T or t < 10:
+ """
+
+""" x = "t" or "T"
+values = input("enter a sentence: ")
+print(values)
+for x in values:
+    print(x)
+count = len(values)
+print(count)
+y= values.split( )
+z = y[0]
+print(y)
+print(z) """
+
+sentence = input("input a sentence: ")
+words = sentence.split()
+letters= words.split( )
+z = letters[0]
+print(letters)
+count = len(words)
+letter_count = len(letters)
+print(count)
+print(letter_count)
